0ecf75a80cbefc311c5d6e4e9700092539cdf4bf
dimasikBurdin <mityabur@gmail.com>
2022-09-02 15:15:03 +0500

    добавлен метод для обработки пост запроса для получения определенного кол-ва контента

--- load-content-onscroll-server/server.js
+++ load-content-onscroll-server/server.js
@@ -5,16 +5,28 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('fuck');
 })
 
-app.get('/data', (req, res) => {
+app.get('/all-data', (req, res) => {
     setTimeout(() => {
         res.send(JSON.stringify(db));
         console.log('data send')        
     }, 1110);
+})
+
+// req -> (startIndex, countItems)
+app.post('/limited-data', (req, res) => {
+    let copyDb = [...db];
+    copyDb = copyDb.splice(req.body.startIndex, req.body.countItems);
+    setTimeout(() => {
+        res.send(JSON.stringify(copyDb))
+        console.log('send limited data', req.body.startIndex, req.body.countItems)
+        
+    }, 1000);
 })
 
 app.listen(port, () => {
