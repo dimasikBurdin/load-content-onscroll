f28337198c8736d2c58088ee5efef85be6718304
dimasikBurdin <mityabur@gmail.com>
2022-09-02 15:15:38 +0500

    прописана логика получения фиксированного кол-ва контента при скролле

--- load-content-onscroll-client/src/components/content/contentContainer.tsx
+++ load-content-onscroll-client/src/components/content/contentContainer.tsx
@@ -10,70 +10,102 @@ type GetData = [{
     index: number
   }];
   
-const getData: () => Promise<GetData> = async () => {
-    let response = await fetch(`${apiUrl}/data`);
+const getAllData: () => Promise<GetData> = async () => {
+    let response = await fetch(`${apiUrl}/all-data`);
     let resData = await response.json() as GetData;
     return resData;
+}
+
+const getLimitedData: (start: number, count: number) => Promise<GetData> = async (start, count) => {
+  let response = await fetch(`${apiUrl}/limited-data`, {
+    method: 'POST',
+    body: JSON.stringify({
+      startIndex: start,
+      countItems: count
+    }),
+    headers: {
+      'content-type': 'application/json'
+    }
+  });
+  let resData = await response.json() as GetData;
+  return resData;
 }
 
 const visibleWindowHeight = window.innerHeight;
 const fullWindowHeight = document.body.offsetHeight;
 
 export const ContentContainer:React.FC = () => {
-    const [showLoading, setShowLoading] = useState<boolean>(false);
-    const [visibleItems, setVisibleItems] = useState<number>(3);
-    const {status, error, data, refetch} = useQuery<GetData, Error>('data', () => getData(), {
-        refetchOnWindowFocus: false,
-        enabled: false
-      });
+  const [currentData, setCurrentData] = useState<GetData>();
+  const [visibleItems, setVisibleItems] = useState<number>(3);
+  const {status, error, data, refetch, isLoading} = useQuery<GetData, Error>(['data', visibleItems - 3, visibleItems], () => getLimitedData(visibleItems - 3, 3), {
+    refetchOnWindowFocus: false,
+    enabled: false
+  });
 
-      
-      window.onscroll = e => {
-        let currentPosition = window.scrollY + visibleWindowHeight;
-        let treshhold = fullWindowHeight - visibleWindowHeight / 4
-        if(currentPosition >= treshhold && visibleItems * 400 < currentPosition) {
-            setShowLoading(true);
-            setTimeout(() => {
-                setVisibleItems(visibleItems + 2);
-                setShowLoading(false)                
-            }, 1000);
-            console.log(122)
-        }
-      }
     
-      useEffect(() => {
-        setTimeout(() => {
-          refetch();
-        }, 0);
-      }, [])
-      
+  window.onscroll = e => {
+    let currentPosition = window.scrollY + visibleWindowHeight;
+    let treshhold = fullWindowHeight - visibleWindowHeight / 4
+    if(currentPosition >= treshhold && visibleItems * 400 < currentPosition) {
+      setVisibleItems(visibleItems + 3);
+    }
+  }
 
-      if(status === 'loading') {
-        return <div className="content-container">
-            <LoadComponent />
-        </div>
-      }
-    
-      if(status === 'error') {
-        return <div>
-          {error.message}
-        </div>
-      }
-    
-      if(status === 'success') {
-        return <div className="content-container">
-          {data?.map((value, index) => {
-            if(index < visibleItems)
-                return <ContentItem 
-                    key={value.value + index}
-                    value={value.value}
-                />
-          })}
-          {showLoading && <LoadComponent />}
-        </div>
-      }
+  useEffect(() => {
+    setTimeout(() => {
+      refetch();
+    }, 0);
+  }, [])
 
-    return <div className="content-container">
-        nothing yet
+  useEffect(() => {
+    if(currentData && data){
+      //@ts-ignore
+      setCurrentData([...currentData, ...data])
+    } else if (data) {
+      setCurrentData(data)
+    }
+  }, [data])
+
+  useEffect(() => {
+    refetch()
+  }, [visibleItems])
+ 
+
+  // if(status === 'loading') {
+  //   return <div className="content-container">
+  //       <LoadComponent />
+  //   </div>
+  // }
+
+  if(status === 'error') {
+    return <div>
+      {error.message}
     </div>
+  }
+
+  // if(status === 'success') {
+  //   return <div className="content-container">
+  //     {currentData?.map((value, index) => {
+  //       if(index < visibleItems)
+  //           return <ContentItem 
+  //               key={value.value + index}
+  //               value={value.value}
+  //           />
+  //       return null;
+  //     })}
+  //     {isLoading && <LoadComponent />}
+  //   </div>
+  // }
+
+  return <div className="content-container">
+    {currentData?.map((value, index) => {
+      if(index < visibleItems)
+          return <ContentItem 
+              key={value.value + index}
+              value={value.value}
+          />
+      return null;
+    })}
+    {isLoading && <LoadComponent />}
+  </div>
 }
